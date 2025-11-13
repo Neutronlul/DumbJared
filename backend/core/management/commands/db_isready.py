9c93ac60b64b9e8563162db60cb8aa77d6eaf444
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2025-11-13 02:03:16 -0800

    Update formatting

--- backend/core/management/commands/db_isready.py
+++ backend/core/management/commands/db_isready.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         db_alias = getattr(settings, "HEALTHCHECK_MIGRATIONS_DB", DEFAULT_DB_ALIAS)
-        
+
         try:
             executor = MigrationExecutor(connections[db_alias])
             plan = executor.migration_plan(executor.loader.graph.leaf_nodes())
@@ -19,11 +19,13 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.SUCCESS("Database is ready and migrated."))
                 sys.exit(0)
             else:
-                self.stderr.write(self.style.WARNING("Database has unapplied migrations."))
+                self.stderr.write(
+                    self.style.WARNING("Database has unapplied migrations.")
+                )
                 sys.exit(1)
         except DatabaseError:
             self.stderr.write(self.style.ERROR("Database is not ready."))
             sys.exit(2)
         except Exception:
-            self.stderr.write(self.style.ERROR("Unexpected error.")) 
+            self.stderr.write(self.style.ERROR("Unexpected error."))
             sys.exit(3)
