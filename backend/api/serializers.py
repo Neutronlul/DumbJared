26941d37c7722cc9cc9588eaf6533b8d772ffb10
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2025-11-25 00:18:43 -0800

    Fix web scraper rate limiting (#55)
    * Update formatting
    
    * Add redis as prod cache, playwright fallback/token fetcher to scraper, and update formatting
    
    * Add comments, improve handling of missing cookie in playwright scraping
    
    * Improve error handling in _fetchPage

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,16 +2,19 @@ from api.models import Team, Glossary
 
 from rest_framework import serializers
 
+
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Team
-        fields = ['name']
+        fields = ["name"]
+
 
 class GlossarySerializer(serializers.HyperlinkedModelSerializer):
     entry = serializers.SerializerMethodField()
+
     class Meta:
         model = Glossary
-        fields = ['entry']
+        fields = ["entry"]
 
     def get_entry(self, obj):
         return f"{obj.acronym} | {obj.definition}"
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,13 +3,13 @@ from rest_framework import viewsets
 from api.models import Team, Glossary
 from api.serializers import TeamSerializer, GlossarySerializer
 
+
 # Create your views here.
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
+
 class GlossaryViewSet(viewsets.ModelViewSet):
     queryset = Glossary.objects.all()
     serializer_class = GlossarySerializer
-
-    
