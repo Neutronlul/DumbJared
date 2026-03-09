3cb0c2fc25c3bb5910a35a64b373adc36987bb53
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2026-03-08 19:53:44 -0700

    Add tests for re-enabling scraping task

--- backend/scraper/tests/test_tasks.py
+++ backend/scraper/tests/test_tasks.py
@@ -1,4 +1,6 @@
 from api.models import Event
+from django.template.defaultfilters import date
+from django_celery_beat.models import PeriodicTask
 from django.utils import timezone
 from model_bakery import baker
 from scraper.tasks import generate_placeholder_event, auto_scrape, reenable_scraping
@@ -30,4 +32,62 @@ class TestAutoScrape:
 
 
 class TestReenableScraping:
-    pass
+    def test_orphaned_placeholder_deleted_and_task_reenabled(self, mocker):
+        today = timezone.localdate()
+
+        mocker.patch("django.utils.timezone.localdate", return_value=today)
+
+        game = baker.make("api.Game", game_type__name="Test Game")
+
+        orphaned_event = baker.make(
+            "api.Event",
+            game=game,
+            date=today - timezone.timedelta(days=1),
+            quizmaster=None,
+        )
+
+        scrape_task = baker.make(
+            "django_celery_beat.PeriodicTask",
+            name=f"{game} - Auto scrape",
+            enabled=False,
+            interval=baker.make("django_celery_beat.IntervalSchedule"),
+        )
+
+        assert Event.objects.filter(pk=orphaned_event.pk).exists()
+        assert not scrape_task.enabled
+
+        reenable_scraping(game_pk=game.pk, task_name=scrape_task.name)
+
+        assert not Event.objects.filter(pk=orphaned_event.pk).exists()
+        scrape_task.refresh_from_db()
+        assert scrape_task.enabled
+
+    def test_no_orphaned_placeholder_but_task_reenabled(self, mocker):
+        today = timezone.localdate()
+
+        mocker.patch("django.utils.timezone.localdate", return_value=today)
+
+        game = baker.make("api.Game", game_type__name="Test Game")
+
+        event = baker.make(
+            "api.Event",
+            game=game,
+            date=today - timezone.timedelta(days=1),
+            quizmaster=baker.make("api.Quizmaster"),
+        )
+
+        scrape_task = baker.make(
+            "django_celery_beat.PeriodicTask",
+            name=f"{game} - Auto scrape",
+            enabled=False,
+            interval=baker.make("django_celery_beat.IntervalSchedule"),
+        )
+
+        assert Event.objects.filter(pk=event.pk).exists()
+        assert not scrape_task.enabled
+
+        reenable_scraping(game_pk=game.pk, task_name=scrape_task.name)
+
+        assert Event.objects.filter(pk=event.pk).exists()
+        scrape_task.refresh_from_db()
+        assert scrape_task.enabled
