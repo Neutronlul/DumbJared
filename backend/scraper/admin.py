7003488ebd668face51eba013947427aa1778da1
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2026-05-20 18:00:15 -0700

    Update ScraperAccountAdmin to display authentication status

--- backend/scraper/admin.py
+++ backend/scraper/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
+from unfold.decorators import display
 
 from scraper import models
 
 
 @admin.register(models.ScraperAccount)
 class ScraperAccountAdmin(ModelAdmin):
-    list_display = ("name", "email", "player_id")
+    list_display = ("name", "email", "is_authenticated")
+    list_display_links = ("name", "email")
 
     readonly_fields = ("token", "player_id")
 
     search_fields = ("name", "email")
+
+    @display(description="Authenticated", ordering="token", boolean=True)
+    def is_authenticated(self, obj: models.ScraperAccount) -> bool:
+        return obj.token != ""
