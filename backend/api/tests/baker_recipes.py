3fbd5ba98b24e5b0673d58fa3cde8e211762b72e
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2026-06-06 11:19:33 -0700

    Fix tests

--- backend/api/tests/baker_recipes.py
+++ backend/api/tests/baker_recipes.py
@@ -1,10 +1,15 @@
-from model_bakery.recipe import Recipe
+from model_bakery.recipe import Recipe, foreign_key
 from model_bakery.utils import seq
 
-from api.models import Event, Quizmaster, Team
+from api.models import Event, Game, Quizmaster, Team, Venue
+from scraper.tests.baker_recipes import geocoded_address
 
 quizmaster = Recipe(Quizmaster, name="John Doe")
 
 team = Recipe(Team, team_id=seq(0))
 
-event = Recipe(Event)
+venue = Recipe(Venue, address=foreign_key(geocoded_address))
+
+game = Recipe(Game, venue=foreign_key(venue))
+
+event = Recipe(Event, game=foreign_key(game))
