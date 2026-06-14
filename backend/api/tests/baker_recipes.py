7d607b46449499f7bff3e0a5313b776211fcd1fd
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2026-06-13 19:08:13 -0700

    Add EventRound model and update related models

--- backend/api/tests/baker_recipes.py
+++ backend/api/tests/baker_recipes.py
@@ -1,10 +1,13 @@
+from faker import Faker
 from model_bakery.recipe import Recipe, foreign_key
 from model_bakery.utils import seq
 
 from api.models import Event, Game, Quizmaster, Team, Venue
 from scraper.tests.baker_recipes import geocoded_address
 
-quizmaster = Recipe(Quizmaster, name="John Doe")
+fake = Faker()
+
+quizmaster = Recipe(Quizmaster, name=fake.name())
 
 team = Recipe(Team, team_id=seq(0))
 
