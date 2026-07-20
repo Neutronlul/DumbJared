b5a4b49b7b41e9bd72dbfe148f2feb1bf06df171
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2026-07-20 03:59:14 -0700

    Refactor MemberQuerySet to use direct imports
    Signed-off-by: Tanner Snow <53470063+Neutronlul@users.noreply.github.com>

--- backend/api/querysets.py
+++ backend/api/querysets.py
@@ -1,13 +1,13 @@
-from django.db import models
+from django.db.models import Avg, QuerySet, Count 
 
 
-class MemberQuerySet(models.QuerySet):
+class MemberQuerySet(QuerySet):
     def with_attendance_count(self) -> MemberQuerySet:
-        return self.annotate(attendance_count=models.Count("event_attendances"))
+        return self.annotate(attendance_count=Count("event_attendances"))
 
     def with_average_score(self) -> MemberQuerySet:
         return self.annotate(
-            average_score=models.Avg(
+            average_score=Avg(
                 "event_attendances__team_event_participation__score",
             ),
         )
